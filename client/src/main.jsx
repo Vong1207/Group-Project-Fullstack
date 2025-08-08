52996aede98d9e89ccf6d4eed4c98901640fa8fb
LinhNguyen <s3999487@rmit.edu.vn>
2025-08-08 17:11:08 +0700

    navbar dev

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client';
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+//pages
 import Home from './pages/home/Home.jsx'
+import NotFound from './pages/partials/NotFound.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element: <Home />,
+    errorElement: <NotFound />
+  }
+])
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- client/src/pages/partials/Navbar.jsx
+++ client/src/pages/partials/Navbar.jsx
@@ -1,9 +1,59 @@
-import './Navbar.css'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
 
 export default function Navbar() {
-    return (
-        <>
-            
-        </>
-    )
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <div className="container-fluid">
+        <NavLink className="navbar-brand" href="#">Navbar</NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+        </button>
+
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+            <NavLink
+              to="/"
+              className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/A"
+              className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+            >
+              A
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/B"
+              className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+            >
+              B
+            </NavLink>
+          </li>
+          </ul>
+           <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            <ul className="navbar-nav">
+            <li className="nav-item">
+            <NavLink
+              to="/login"
+              className={({ isActive }) => `nav-link ${isActive ? 'active' : ''}`}
+            >
+              Login/signup
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+    </nav>
+  );
 }
