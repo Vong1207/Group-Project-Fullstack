6dcfbffd3ecea389639d1b46859a5ef97eb02b8d
Steven's macbook <macbook@Stevens-Macbook.local>
2025-08-08 20:39:52 +0700

    fix Footer

--- client/src/pages/partials/Footer.jsx
+++ client/src/pages/partials/Footer.jsx
@@ -2,47 +2,47 @@ import './Footer.css'
 
 export default function Footer() {
     return (
-        <footer className="footer bg-dark text-light pt-5 pb-3" style={{boxShadow: '0 2px 16px rgba(0,0,0,0.15)', marginTop: '40px', marginInline: '30px', borderRadius: '30px'}}>
+        <footer className="footer bg-dark text-light pt-5 pb-3" style={{boxShadow: '0 2px 16px rgba(0,0,0,0.15)', marginTop: '40px'}}>
             <div className="container">
                 <div className="row gy-4">
                     <div className="col-md-3">
-                        <h5 className="fw-bold mb-3">Ecommerce Shop</h5>
+                        <h5 className="mb-3 footer-title-main">Ecommerce Shop</h5>
                         <p className="mb-1">RMIT University School, Nguyen Van Linh Street, District 7, Ho Chi Minh.</p>
                         <br />
                         <div className="mb-1">123456780</div>
                         <div>1234567890</div>
                     </div>
-                    <div className="col-md-2">
-                        <h5 className="fw-bold mb-3">Menu</h5>
+                    <div className="col-md-2 footer-col-sub">
+                        <h5 className="mb-3 footer-title-sub">Menu</h5>
                         <ul className="list-unstyled">
-                            <li><a href="#" className="text-light text-decoration-none">Home</a></li>
-                            <li><a href="#" className="text-light text-decoration-none">About</a></li>
-                            <li><a href="#" className="text-light text-decoration-none">Contact</a></li>
+                        <li><a href="#" className="text-decoration-none">Home</a></li>
+                        <li><a href="#" className="text-decoration-none">About</a></li>
+                        <li><a href="#" className="text-decoration-none">Contact</a></li>
                         </ul>
                     </div>
-                    <div className="col-md-2">
-                        <h5 className="fw-bold mb-3">More</h5>
+                    <div className="col-md-2 footer-col-sub">
+                        <h5 className="mb-3 footer-title-sub">More</h5>
                         <ul className="list-unstyled">
-                            <li><a href="#" className="text-light text-decoration-none">Landing Pages</a></li>
-                            <li><a href="#" className="text-light text-decoration-none">FAQs</a></li>
+                        <li><a href="#" className="text-decoration-none">Landing Pages</a></li>
+                        <li><a href="#" className="text-decoration-none">FAQs</a></li>
                         </ul>
                     </div>
-                    <div className="col-md-2">
-                        <h5 className="fw-bold mb-3">Categories</h5>
+                    <div className="col-md-2 footer-col-sub">
+                        <h5 className="mb-3 footer-title-sub">Categories</h5>
                         <ul className="list-unstyled">
-                            <li><a href="#" className="text-light text-decoration-none">SmartPhone</a></li>
-                            <li><a href="#" className="text-light text-decoration-none">Shoes</a></li>
-                            <li><a href="#" className="text-light text-decoration-none">Clothes</a></li>
-                            <li><a href="#" className="text-light text-decoration-none">Carousels</a></li>
+                        <li><a href="#" className="text-decoration-none">SmartPhone</a></li>
+                        <li><a href="#" className="text-decoration-none">Shoes</a></li>
+                        <li><a href="#" className="text-decoration-none">Clothes</a></li>
+                        <li><a href="#" className="text-decoration-none">Carousels</a></li>
                         </ul>
                     </div>
-                    <div className="col-md-3">
-                        <h5 className="fw-bold mb-3">Social Media Links</h5>
+                    <div className="col-md-3 footer-col-sub">
+                        <h5 className="mb-3 footer-title-sub">Social Media Links</h5>
                         <div className="d-flex gap-3">
-                            <a href="#" className="text-light fs-4"><i className="fab fa-facebook"></i></a>
-                            <a href="#" className="text-light fs-4"><i className="fab fa-pinterest"></i></a>
-                            <a href="#" className="text-light fs-4"><i className="fab fa-instagram"></i></a>
-                            <a href="#" className="text-light fs-4"><i className="fab fa-linkedin"></i></a>
+                            <a href="#"><i className="fab fa-facebook"></i></a>
+                            <a href="#"><i className="fab fa-pinterest"></i></a>
+                            <a href="#"><i className="fab fa-instagram"></i></a>
+                            <a href="#"><i className="fab fa-linkedin"></i></a>
                         </div>
                     </div>
                 </div>
