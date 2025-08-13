31ce544e01dda8c4b6d0b6809a262f4e902eb375
Tran Gia Vong <s4012094@rmit.edu.vn>
2025-08-13 13:02:42 +0700

    Added Space for Sample Products, Deleted not-needed Files, Add Product Folder to Store Images

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,6 +4,18 @@ import Footer from '../partials/Footer.jsx'
 import PromotionCarousel from './PromotionCarousel.jsx'
 import CategoryCatalog from './CategoryCatalog.jsx'
 
+const sampleProducts = [
+    // Example Sample Product
+    {
+        name: '',
+        price: 0,
+        category: '',
+        mainImageUrl: '',
+        rating: 0
+    },
+
+];
+
 export default function Home() {
     return (
         <>
