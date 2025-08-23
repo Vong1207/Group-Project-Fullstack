52b5dfb2e3c7cf2c0c559160d915e500b9f03769
Steven's macbook <macbook@Stevens-Macbook.local>
2025-08-23 13:49:10 +0700

    fix login BE

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,28 +4,28 @@ import { comparePasswords } from "../bcrypt/bcrypt.js";
 
 const router = express.Router();
 
-// Đăng nhập
+// Log in
 router.post("/signin", async (req, res) => {
     const { username, password } = req.body;
     try {
         const user = await User.findOne({ username });
         if (!user) {
-            return res.status(400).json({ message: "Sai tài khoản hoặc mật khẩu" });
+            return res.status(400).json({ message: "Incorrect username or password. Check again!" });
         }
         const isMatch = await comparePasswords(password, user.password);
         if (!isMatch) {
-            return res.status(400).json({ message: "Sai tài khoản hoặc mật khẩu" });
+            return res.status(400).json({ message: "Incorrect username or password" });
         }
-        // Lưu thông tin user vào session
+        // Save user info in session
         req.session.user = {
             _id: user._id,
             username: user.username,
             role: user.role,
             displayName: user.displayName || user.businessName
         };
-        res.json({ message: "Đăng nhập thành công", user: req.session.user });
+        res.json({ message: "Successfully Log in", user: req.session.user });
     } catch (err) {
-        res.status(500).json({ message: "Lỗi server" });
+        res.status(500).json({ message: "Server Internal" });
     }
 });
 
