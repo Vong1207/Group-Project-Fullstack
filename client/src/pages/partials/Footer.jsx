5cb014e4d9aae77708e6fdd84e7d6fefbce6ea2d
Steven's macbook <macbook@Stevens-Macbook.local>
2025-09-10 17:23:52 +0700

    logo and update brand name

--- client/src/pages/partials/Footer.jsx
+++ client/src/pages/partials/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
             <div className='container px-sm-0'>
                 <div className='row mx-0 py-5'>
                     <div className='col-md-7 col-12'>
-                        <h1 className='fs-3 mb-4'>Brand Name</h1>
+                        <h1 className='brandName fs-3 mb-4'>CARTELLO</h1>
                         <p><small><span className='bi bi-geo-alt-fill me-1'></span> 702 Nguyen Van Linh boulevard, Tan Hung ward, Ho Chi Minh city, Vietnam</small></p>
                         <p><small><span className='bi bi-telephone-fill me-1'></span> (+84) 28 3776 1369</small></p>
                         <p><small><span className='bi bi-envelope-fill me-1'></span> example@gmail.com</small></p>
@@ -48,7 +48,7 @@ export default function Footer() {
                 <hr />
 
                 <div className='row text-center'>
-                    <p>© 2025 by Brand Name</p>
+                    <p>© 2025 by Cartello</p>
                 </div>
             </div>
         </footer>
