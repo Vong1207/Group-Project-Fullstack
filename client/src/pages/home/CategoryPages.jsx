0e723faea185fa600c98f2a4dc91692b286fe9cb
Khoa <nguyenkhoa231205@gmail.com>
2025-09-16 19:11:24 +0700

    Add comment

--- client/src/pages/home/CategoryPages.jsx
+++ client/src/pages/home/CategoryPages.jsx
@@ -2,8 +2,8 @@
 // # Course: COSC2769 - Full Stack Development
 // # Semester: 2025B
 // # Assessment: Assignment 02
-// # Author: Nguyen Vu Linh,
-// # ID: 3999487 */
+// # Author: Nguyen Vu Linh, Nguyen Minh Nguyen Khoa
+// # ID: 3999487, 4033604 */
 import { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
